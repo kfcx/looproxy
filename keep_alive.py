db82bd63bc569cd71aad59d86686ee398257a8dc
Naihe <64341459+239144498@users.noreply.github.com>
2024-12-15 14:59:15 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -2,12 +2,22 @@
 import time
 import httpx
 import os
+import threading
 
-url = f"https://{os.environ.get('PROJECT_DOMAIN')}/health"
-while True:
-    try:
-        httpx.get(url).close()
-        print("Keeping alive...")
-    except:
-        print("Failed to keep alive")
-    time.sleep(5 * 60)  # 每5分钟请求一次
+def function_to_run():
+    count = 0
+    url = f"https://{os.environ.get('PROJECT_DOMAIN')}/health"
+    while True:
+        if count > 30:
+            break
+        try:
+            httpx.get(url).close()
+            print("Keeping alive...")
+        except:
+            print("Failed to keep alive")
+            count += 1
+        time.sleep(5 * 60)  # 每5分钟请求一次
+
+thread = threading.Thread(target=function_to_run)
+thread.start()
+# thread.join()
